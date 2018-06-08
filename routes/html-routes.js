ca92664df8e697df3dd89a9a004826c6e9144fb3
Oscar Villalta <bagglerock@gmail.com>
2018-06-08 00:28:22 -0400

    got axios working and its pretty cool.  scrapes google news but doesnt look like each article has an image =(

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,49 +1,61 @@
 const express = require("express");
-const request = require("request");
+
 // Our scraping tools
 // It works on the client and on the server
+const axios = require("axios");
 const cheerio = require("cheerio");
+
+//Import mongoose
+const mongoose = require("mongoose");
+
+// Import the models
+const db = require("../models");
+
+mongoose.connect("mongodb://localhost/scrapoogle");
 
 module.exports = function (app) {
 
 
     // A GET route for scraping a site
     app.get("/", function (req, res) {
-        request("https://old.reddit.com/r/webdev/", function (error, response, html) {
 
-            // Load the HTML into cheerio and save it to a variable
-            // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-            var $ = cheerio.load(html);
+        axios.get('https://news.google.com/')
 
-            // An empty array to save the data that we'll scrape
-            var results = [];
-            var hbsObject = {};
+            .then(function (response) {
+                let $ = cheerio.load(response.data);
 
-            // With cheerio, find each p-tag with the "title" class
-            // (i: iterator. element: the current element)
-            $("p.title").each(function (i, element) {
+                $("article a").each(function (i, element) {
+                    // console.log(element);
 
-                // Save the text of the element in a "title" variable
-                var title = $(element).text();
+                    let result = {};
 
-                // In the currently selected element, look at its child elements (i.e., its a-tags),
-                // then save the values for any "href" attributes that the child elements may have
-                var link = $(element).children().attr("href");
+                    result.title = $(this)
+                        .children("span")
+                        .text();
+                    result.link = $(this)
+                        .attr("href");
 
-                // Save these results in an object that we'll push into the results array we defined earlier
-                results.push(
-                    {
-                        title: title,
-                        link: link
+                    if (result.title !== "" && result.link !== "") {
+                        db.Article.create(result)
+                            .then(function (dbArticle) {
+                                // View the added result in the console
+                                console.log(dbArticle);
+                            })
+                            .catch(function (err) {
+                                // If an error occurred, send it to the client
+                                return res.json(err);
+                            });
+
                     }
-                );
-                hbsObject = {
-                    results: results
-                }
+
+                })
+                console.log("scraping done.");
+            })
+
+            .catch(function (error) {
+                console.log(error);
             });
-            res.render("index", hbsObject);
-        });
-        
+
     });
 
 
