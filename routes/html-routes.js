d28900f18e5f1613d932869b93b06f20f3ccde85
Oscar Villalta <bagglerock@gmail.com>
2018-06-09 10:27:28 -0400

    added the sort last/first thing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -60,7 +60,7 @@ module.exports = function (app) {
     // Route for getting all Articles from the db
     app.get("/", function (req, res) {
         // Grab every document in the Articles collection
-        db.Article.find({})
+        db.Article.find({}).sort({_id:-1})
             .then(function (dbArticle) {
                 // If we were able to successfully find Articles, send them back to the client
                 let hbsObject = {
