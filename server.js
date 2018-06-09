ccfc7025f2b656fc39e8d0b4e86bff21acf9b844
Oscar Villalta <bagglerock@gmail.com>
2018-06-09 11:46:10 -0400

    added port to the server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
